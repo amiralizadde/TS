41cc16776ec4807825ddd5488988b96e7fbfb917
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-09-21 19:21:56 +0330

    update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let  user = 12;
+var count = 12;
+var lastName = 'amir';
+var isLogin = true;
+var user = {
+    id: 1,
+    name: 'amir',
+    lastName: "Alizade"
+};
+console.log('user : ', user);
+var newUser = ['amir', 12, false];
--- app.ts
+++ app.ts
@@ -1,2 +1,15 @@
-let user = 12;
-let name = 'amir';
+let count:number = 12;
+
+let lastName:string = 'amir';
+
+let isLogin:boolean = true;
+
+let user:{id:number , name:string , lastName : string}={
+    id:1,
+    name:'amir',
+    lastName:"Alizade"
+}
+
+console.log('user : ' , user);
+
+let newUser:(string|number|boolean)[]=['amir' , 12 , false]
