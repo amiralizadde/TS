47b38b9d4636287289f020212f088d9c80edb98e
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-10-05 14:23:59 +0330

    create login

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ var user = {
     age: 28
 };
 var data = ['amir', 1];
+function login(userName, password) {
+    return true;
+}
+login('amir alizade', 1234);
+//literal type
--- app.ts
+++ app.ts
@@ -9,3 +9,10 @@ let user:{id:number , name:string , age:number}={
 }
 
 let data:[string ,number]=['amir' ,1]
+
+function login(userName:string , password:number):boolean{
+    return true
+}
+login('amir alizade' , 1234)
+
+//literal type
