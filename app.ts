bf2f5d4250b3f8e6388fdf5f0114b31006571eb3
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-10-07 14:12:55 +0330

    update login

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
-const userName:string = 'amir';
+// const userName:string = 'amir';
 
-let isLogin:boolean = false
+// let isLogin:boolean = false
 
-let user:{id:number , name:string , age:number}={
-    id:1,
-    name:"amir",
-    age:28
-}
+// let user:{id:number , name:string , age:number}={
+//     id:1,
+//     name:"amir",
+//     age:28
+// }
 
-let data:[string ,number]=['amir' ,1]
+// let data:[string ,number]=['amir' ,1]
 
-function login(userName:string , password:number):boolean{
-    return true
-}
-login('amir alizade' , 1234)
+// function login(userName:string , password:number):boolean{
+//     return true
+// }
+// login('amir alizade' , 1234)
 
 //literal type
