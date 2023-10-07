e07a1a56b2ed4648242f309a99ffd30740719ebc
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-10-07 14:36:39 +0330

    learning const cast in dom

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var userName = 'amir';
-var isLogin = false;
-var user = {
-    id: 1,
-    name: "amir",
-    age: 28
-};
-var data = ['amir', 1];
+// const userName:string = 'amir';
+// let isLogin:boolean = false
+// let user:{id:number , name:string , age:number}={
+//     id:1,
+//     name:"amir",
+//     age:28
+// }
+// let data:[string ,number]=['amir' ,1]
+//learning const cast in dom 
+// syntax1 : 
+var userName = document.querySelector('.userName');
+console.log(userName.value);
+// syntax2
+var buttonElement = document.querySelector('.btn');
+console.log(buttonElement.innerHTML);
+//syntax 3
+var buttonElement2 = document.querySelector('.btn');
+console.log(buttonElement.innerHTML);
--- app.ts
+++ app.ts
@@ -1,11 +1,33 @@
-const userName:string = 'amir';
+// const userName:string = 'amir';
 
-let isLogin:boolean = false
+// let isLogin:boolean = false
 
-let user:{id:number , name:string , age:number}={
-    id:1,
-    name:"amir",
-    age:28
-}
+// let user:{id:number , name:string , age:number}={
+//     id:1,
+//     name:"amir",
+//     age:28
+// }
 
-let data:[string ,number]=['amir' ,1]
+// let data:[string ,number]=['amir' ,1]
+
+//learning const cast in dom 
+
+// syntax1 : 
+
+const userName = document.querySelector('.userName') as HTMLInputElement;
+
+console.log(userName.value);
+
+// syntax2
+
+const buttonElement = <HTMLButtonElement>document.querySelector('.btn')
+
+console.log(buttonElement.innerHTML);
+
+//syntax 3
+
+const buttonElement2 = document.querySelector('.btn')
+
+console.log((buttonElement as HTMLButtonElement).innerHTML);
+
+
