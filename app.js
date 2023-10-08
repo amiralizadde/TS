3ae53ed61b2d6d6d896c6a6a986552a0118d291b
amiralizadde <75627484+amiralizadde@users.noreply.github.com>
2023-10-08 19:16:10 +0330

    update

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var userName = 'amir';
-var isLogin = false;
-var user = {
-    id: 1,
-    name: "amir",
-    age: 28
-};
-var data = ['amir', 1];
-function login(userName, password) {
-    return true;
-}
-login('amir alizade', 1234);
+"use strict";
+// const userName:string = 'amir';
+// let isLogin:boolean = false
+// let user:{id:number , name:string , age:number}={
+//     id:1,
+//     name:"amir",
+//     age:28
+// }
+// let data:[string ,number]=['amir' ,1]
+// function login(userName:string , password:number):boolean{
+//     return true
+// }
+// login('amir alizade' , 1234)
 //literal type
+let changeVariable;
